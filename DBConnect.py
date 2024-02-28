c90935c6db5737bfaa9e62f1bcfd3e58d4760184
thepopeofpoland <93283314+thepopeofpoland@users.noreply.github.com>
2024-02-28 13:45:09 -0600

    test

--- DBConnect.py
+++ DBConnect.py
@@ -9,6 +9,7 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS family(
                 numMembers int
             )''')
 
+print("hello")
 cursor.execute("INSERT INTO family (sirName, numMembers) VALUES (?, ?)", ('Pope', 3))
 # Inserting data into the family table
 
