c63700ac5e5371e512ae05eebfd7aedd8eac2306
thepopeofpoland <93283314+thepopeofpoland@users.noreply.github.com>
2024-04-12 18:25:22 -0500

    Started on unittests for dbconnect. Must do more research for other functions

--- testdbconnect.py
+++ testdbconnect.py
@@ -1,11 +1,20 @@
 import unittest
 from unittest.mock import MagicMock
-from dbconnect import retrieve_family_data, connection, family
+import sqlalchemy as db
+import dbconnect
+from dbconnect import retrieve_family_data, retrieve_cal_dates, insert_family, update_family, connection, family
 
 
-class TestRetrieveFamilyData(unittest.TestCase):
+class TestDBConnect(unittest.TestCase):
+    def setUp(self):
+        self.mock_db = MagicMock()
+        self.mock_connection = MagicMock()
+        self.mock_execute = MagicMock()
+        self.mock_commit = MagicMock()
+        connection.execute = self.mock_execute
+        connection.commit = self.mock_commit
+
     def test_retrieve_family_data(self):
-        # Create a MagicMock for the result of the execute method
         mock_result = MagicMock()
         mock_result.fetchall.return_value = [
             ('Smith', 5),
@@ -23,3 +32,23 @@ class TestRetrieveFamilyData(unittest.TestCase):
         mock_result.fetchall.assert_called_once()
 
         self.assertEqual(result, expected_output)
+
+    def test_retrieve_cal_dates(self,):
+        mock_result_temp = MagicMock()
+        mock_result_temp.fetchall.return_value = [
+            ('2024-04-12', 'Smith'),
+            ('2024-04-13', 'Doe')
+        ]
+
+        connection.execute = MagicMock(return_value=mock_result_temp)
+
+        result = retrieve_cal_dates()
+
+        expected_result = [('2024-04-12', 'Smith'), ('2024-04-13', 'Doe')]
+
+        connection.execute.assert_called_once()
+
+        mock_result_temp.fetchall.assert_called_once()
+
+        self.assertEqual(result, expected_result)
+
